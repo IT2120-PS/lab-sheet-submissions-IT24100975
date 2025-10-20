791e34dafd8580652cb358a9034c318d27abbff2
IT24100975 <it24100975@my.sliit.lk>
2025-10-20 16:32:20 +0530

    IT24100975

---
+++ Lab Sheet 10/IT24100975.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\my pc\\OneDrive\\Desktop\\PS_Lab10")
+# Counts
+obs <- c(A=120, B=95, C=85, D=100)
+
+# Chi-square goodness-of-fit test for equal probabilities
+chisq.test(obs, p = rep(1/4, 4))  # expected = 100 each
+
+# If you want to see expected and residuals:
+out <- chisq.test(obs, p = rep(1/4, 4))
+out$expected
+out$residuals
+
